b5b67cacbb261a9203a96439846578aea783ec7e
Minhajul-Shobuj <mdminhajulislamshobuj@gmail.com>
2021-10-04 02:02:46 +0600

    adding-route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
-
+import About from './Component/About/About';
+import Blog from './Component/Blog/Blog';
+import Courses from './Component/Courses/Courses';
+import Home from './Component/Home/Home';
+import NotFound from './Component/NotFound/NotFound';
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="/course">
+            <Courses></Courses>
+          </Route>
+          <Route path="/blog">
+            <Blog></Blog>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
