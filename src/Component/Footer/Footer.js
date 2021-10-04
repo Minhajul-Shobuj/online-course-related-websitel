f75f719fceac1385ff44b90e5cb186bd3db9a7c2
Minhajul-Shobuj <mdminhajulislamshobuj@gmail.com>
2021-10-04 20:05:17 +0600

    aded

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="text-primary container d-flex text-center justify-content-center mt-4 p-4">
-            <h6>Copyright: Learn-Quran Academy</h6>
+        <div className="text-primary container text-center justify-content-center mt-4 p-4">
+            <div>
+                <h3>Contact us</h3>
+                <h6>E-mail: learnQuran@gmail.com</h6>
+                <h6>Phone: +880175075155</h6>
+            </div>
+            <h6>[Copyright: Learn-Quran Academy]</h6>
         </div>
     );
 };
